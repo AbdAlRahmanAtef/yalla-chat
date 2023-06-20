3a84a1b8655b56340715f01d971b3ddb7dc808d6
Abdalrahman Atef <abdalrhman.atef.ramadan@gmail.com>
2023-06-20 09:40:07 +0300

    modified

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_CONNECTION_URL,
     methods: ["GET", "POST"],
   },
 });
